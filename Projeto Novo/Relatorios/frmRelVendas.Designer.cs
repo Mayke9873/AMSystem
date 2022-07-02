47a39616757740fc05f38e454c0f1790482eff0d
mayke.s.sousa@gmail.com <mayke9873>
2022-07-02 11:47:00 -0300

    Filtro relatório com inicio fixo.

--- Projeto Novo/Relatorios/frmRelVendas.Designer.cs
+++ Projeto Novo/Relatorios/frmRelVendas.Designer.cs
@@ -29,7 +29,8 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource2 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmRelVendas));
             this.vendaBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.projetodbDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.projeto_dbDataSet = new Projeto_Novo.projeto_dbDataSet();
@@ -39,6 +40,7 @@
             this.dtpFinal = new System.Windows.Forms.DateTimePicker();
             this.chkTodos = new System.Windows.Forms.CheckBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.tbnFiltrar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.vendaBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.projetodbDataSetBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.projeto_dbDataSet)).BeginInit();
@@ -62,9 +64,10 @@
             // rpvVendas
             // 
             this.rpvVendas.Dock = System.Windows.Forms.DockStyle.Fill;
-            reportDataSource2.Name = "Venda_DataSet";
-            reportDataSource2.Value = this.vendaBindingSource;
-            this.rpvVendas.LocalReport.DataSources.Add(reportDataSource2);
+            this.rpvVendas.IsDocumentMapWidthFixed = true;
+            reportDataSource1.Name = "Venda_DataSet";
+            reportDataSource1.Value = this.vendaBindingSource;
+            this.rpvVendas.LocalReport.DataSources.Add(reportDataSource1);
             this.rpvVendas.LocalReport.ReportEmbeddedResource = "Projeto_Novo.Relatorios.Vendas.rdlc";
             this.rpvVendas.Location = new System.Drawing.Point(0, 0);
             this.rpvVendas.Name = "rpvVendas";
@@ -85,18 +88,18 @@
             // 
             this.dtpInicial.Enabled = false;
             this.dtpInicial.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dtpInicial.Location = new System.Drawing.Point(405, 3);
+            this.dtpInicial.Location = new System.Drawing.Point(398, 3);
             this.dtpInicial.Name = "dtpInicial";
-            this.dtpInicial.Size = new System.Drawing.Size(83, 20);
+            this.dtpInicial.Size = new System.Drawing.Size(79, 20);
             this.dtpInicial.TabIndex = 1;
             // 
             // dtpFinal
             // 
             this.dtpFinal.Enabled = false;
             this.dtpFinal.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dtpFinal.Location = new System.Drawing.Point(522, 3);
+            this.dtpFinal.Location = new System.Drawing.Point(507, 3);
             this.dtpFinal.Name = "dtpFinal";
-            this.dtpFinal.Size = new System.Drawing.Size(83, 20);
+            this.dtpFinal.Size = new System.Drawing.Size(79, 20);
             this.dtpFinal.TabIndex = 2;
             // 
             // chkTodos
@@ -106,27 +109,39 @@
             this.chkTodos.Checked = true;
             this.chkTodos.CheckState = System.Windows.Forms.CheckState.Checked;
             this.chkTodos.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
-            this.chkTodos.Location = new System.Drawing.Point(610, 5);
+            this.chkTodos.Location = new System.Drawing.Point(591, 5);
             this.chkTodos.Name = "chkTodos";
             this.chkTodos.Size = new System.Drawing.Size(56, 17);
             this.chkTodos.TabIndex = 3;
             this.chkTodos.Text = "Todos";
             this.chkTodos.UseVisualStyleBackColor = false;
+            this.chkTodos.CheckedChanged += new System.EventHandler(this.chkTodos_CheckedChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(494, 7);
+            this.label1.Location = new System.Drawing.Point(481, 7);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(22, 13);
             this.label1.TabIndex = 4;
             this.label1.Text = "até";
+            // 
+            // tbnFiltrar
+            // 
+            this.tbnFiltrar.Image = ((System.Drawing.Image)(resources.GetObject("tbnFiltrar.Image")));
+            this.tbnFiltrar.Location = new System.Drawing.Point(644, 2);
+            this.tbnFiltrar.Name = "tbnFiltrar";
+            this.tbnFiltrar.Size = new System.Drawing.Size(31, 23);
+            this.tbnFiltrar.TabIndex = 5;
+            this.tbnFiltrar.UseVisualStyleBackColor = true;
+            this.tbnFiltrar.Click += new System.EventHandler(this.tbnFiltrar_Click);
             // 
             // frmRelVendas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(684, 551);
+            this.Controls.Add(this.tbnFiltrar);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.chkTodos);
             this.Controls.Add(this.dtpFinal);
@@ -155,5 +170,6 @@
         private System.Windows.Forms.DateTimePicker dtpFinal;
         private System.Windows.Forms.CheckBox chkTodos;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button tbnFiltrar;
     }
 }
--- Projeto Novo/Relatorios/frmRelVendas.cs
+++ Projeto Novo/Relatorios/frmRelVendas.cs
@@ -18,11 +18,69 @@ namespace Projeto_Novo.Relatorios
             InitializeComponent();
         }
 
+        string dtInicial = "1899-01-01", dtFinal = "3000-01-01";
+
+        //private void GeraRelatrio()
+        //{
+        //    // TODO: esta linha de código carrega dados na tabela 'projeto_dbDataSet.venda'. Você pode movê-la ou removê-la conforme necessário.
+        //    this.vendaTableAdapter.Fill(this.projeto_dbDataSet.venda);
+        //    this.rpvVendas.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("DtInicial", dtpInicial.Text));
+        //    this.rpvVendas.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("DtFinal", dtpFinal.Text));
+        //    this.rpvVendas.RefreshReport();
+
+        //    if (chkTodos.Checked)
+        //    {
+        //        dtpFinal.Enabled = false;
+        //        dtpInicial.Enabled = false;
+        //    }
+        //    else
+        //    {
+        //        dtpFinal.Enabled = true;
+        //        dtpInicial.Enabled = true;
+        //    }
+        //}
+
         private void frmRelVendas_Load(object sender, EventArgs e)
         {
-            // TODO: esta linha de código carrega dados na tabela 'projeto_dbDataSet.venda'. Você pode movê-la ou removê-la conforme necessário.
             this.vendaTableAdapter.Fill(this.projeto_dbDataSet.venda);
+            this.rpvVendas.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("DtInicial", dtInicial));
+            this.rpvVendas.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("DtFinal", dtFinal));
+            this.rpvVendas.SetDisplayMode(Microsoft.Reporting.WinForms.DisplayMode.PrintLayout);
             this.rpvVendas.RefreshReport();
+        }
+
+        private void chkTodos_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkTodos.Checked)
+            {
+                dtpFinal.Enabled = false;
+                dtpInicial.Enabled = false;
+            }
+            else
+            {
+                dtpFinal.Enabled = true;
+                dtpInicial.Enabled = true;
+            }
+        }
+
+        private void tbnFiltrar_Click(object sender, EventArgs e)
+        {
+            if (chkTodos.Checked)
+            {
+                this.vendaTableAdapter.Fill(this.projeto_dbDataSet.venda);
+                this.rpvVendas.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("DtInicial", dtInicial));
+                this.rpvVendas.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("DtFinal", dtFinal));
+                this.rpvVendas.SetDisplayMode(Microsoft.Reporting.WinForms.DisplayMode.PrintLayout);
+                this.rpvVendas.RefreshReport();
+            }
+            else
+            {
+                this.vendaTableAdapter.Fill(this.projeto_dbDataSet.venda);
+                this.rpvVendas.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("DtInicial", dtpInicial.Text));
+                this.rpvVendas.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("DtFinal", dtpFinal.Text));
+                this.rpvVendas.SetDisplayMode(Microsoft.Reporting.WinForms.DisplayMode.PrintLayout);
+                this.rpvVendas.RefreshReport();
+            }
         }
     }
 }
