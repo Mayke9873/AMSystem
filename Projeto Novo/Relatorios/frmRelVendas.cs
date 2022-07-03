4e52d2b2ab26a80fe1372cdd85f6b00b48ce9b50
mayke.s.sousa@gmail.com <mayke9873>
2022-07-03 19:26:55 -0300

    Ajustado código (filtro relatório).

--- Projeto Novo/Relatorios/frmRelVendas.cs
+++ Projeto Novo/Relatorios/frmRelVendas.cs
@@ -18,37 +18,6 @@ namespace Projeto_Novo.Relatorios
             InitializeComponent();
         }
 
-        string dtInicial = "1899-01-01", dtFinal = "3000-01-01";
-
-        //private void GeraRelatrio()
-        //{
-        //    // TODO: esta linha de código carrega dados na tabela 'projeto_dbDataSet.venda'. Você pode movê-la ou removê-la conforme necessário.
-        //    this.vendaTableAdapter.Fill(this.projeto_dbDataSet.venda);
-        //    this.rpvVendas.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("DtInicial", dtpInicial.Text));
-        //    this.rpvVendas.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("DtFinal", dtpFinal.Text));
-        //    this.rpvVendas.RefreshReport();
-
-        //    if (chkTodos.Checked)
-        //    {
-        //        dtpFinal.Enabled = false;
-        //        dtpInicial.Enabled = false;
-        //    }
-        //    else
-        //    {
-        //        dtpFinal.Enabled = true;
-        //        dtpInicial.Enabled = true;
-        //    }
-        //}
-
-        private void frmRelVendas_Load(object sender, EventArgs e)
-        {
-            this.vendaTableAdapter.Fill(this.projeto_dbDataSet.venda);
-            this.rpvVendas.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("DtInicial", (string)null));
-            this.rpvVendas.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("DtFinal", (string)null));
-            this.rpvVendas.SetDisplayMode(Microsoft.Reporting.WinForms.DisplayMode.PrintLayout);
-            this.rpvVendas.RefreshReport();
-        }
-
         private void chkTodos_CheckedChanged(object sender, EventArgs e)
         {
             if (chkTodos.Checked)
@@ -63,8 +32,10 @@ namespace Projeto_Novo.Relatorios
             }
         }
 
-        private void tbnFiltrar_Click(object sender, EventArgs e)
+        private void GeraRelatrio()
         {
+            // TODO: esta linha de código carrega dados na tabela 'projeto_dbDataSet.venda'. Você pode movê-la ou removê-la conforme necessário.
+
             if (chkTodos.Checked)
             {
                 this.vendaTableAdapter.Fill(this.projeto_dbDataSet.venda);
@@ -81,6 +52,16 @@ namespace Projeto_Novo.Relatorios
                 this.rpvVendas.SetDisplayMode(Microsoft.Reporting.WinForms.DisplayMode.PrintLayout);
                 this.rpvVendas.RefreshReport();
             }
+        }
+
+        private void frmRelVendas_Load(object sender, EventArgs e)
+        {
+            this.GeraRelatrio();
+        }
+
+        private void tbnFiltrar_Click(object sender, EventArgs e)
+        {
+            this.GeraRelatrio();
         }
     }
 }
