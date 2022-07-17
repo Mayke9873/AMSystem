c45f5595e9b1690f957b029cf2058faf4c0135a8
mayke.s.sousa@gmail.com <mayke9873>
2022-07-17 20:12:01 -0300

    Compras.

--- Projeto Novo/Compras/ConsCompras.Designer.cs
+++ Projeto Novo/Compras/ConsCompras.Designer.cs
@@ -29,47 +29,35 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmConsCompras));
-            this.dgvCompras = new System.Windows.Forms.DataGridView();
-            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
-            this.toolStripSeparator = new System.Windows.Forms.ToolStripSeparator();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.tsCompra = new System.Windows.Forms.ToolStrip();
             this.tsbtnAddCompra = new System.Windows.Forms.ToolStripButton();
             this.tsbtnEditCompra = new System.Windows.Forms.ToolStripButton();
             this.tsbtnCancelar = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator = new System.Windows.Forms.ToolStripSeparator();
             this.tsbtnImprimir = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.tsbtnSair = new System.Windows.Forms.ToolStripButton();
-            this.lblFornecedor = new System.Windows.Forms.Label();
             this.txtPesquisa = new System.Windows.Forms.TextBox();
-            this.pnlConsCompras = new System.Windows.Forms.Panel();
-            this.mtxDtFinal = new System.Windows.Forms.MaskedTextBox();
-            this.txtDtInicial = new System.Windows.Forms.MaskedTextBox();
+            this.lblFornecedor = new System.Windows.Forms.Label();
             this.lblPeriodo = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvCompras)).BeginInit();
+            this.mtxDtInicial = new System.Windows.Forms.MaskedTextBox();
+            this.mtxDtFinal = new System.Windows.Forms.MaskedTextBox();
+            this.pnlConsCompras = new System.Windows.Forms.Panel();
+            this.dgvCompras = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fornecedor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.vendedor = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.tsCompra.SuspendLayout();
             this.pnlConsCompras.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCompras)).BeginInit();
             this.SuspendLayout();
-            // 
-            // dgvCompras
-            // 
-            this.dgvCompras.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.dgvCompras.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvCompras.Location = new System.Drawing.Point(3, 58);
-            this.dgvCompras.Name = "dgvCompras";
-            this.dgvCompras.Size = new System.Drawing.Size(977, 529);
-            this.dgvCompras.TabIndex = 0;
-            this.dgvCompras.TabStop = false;
-            // 
-            // toolStripSeparator1
-            // 
-            this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 29);
-            // 
-            // toolStripSeparator
-            // 
-            this.toolStripSeparator.Name = "toolStripSeparator";
-            this.toolStripSeparator.Size = new System.Drawing.Size(6, 29);
             // 
             // tsCompra
             // 
@@ -119,6 +107,11 @@
             this.tsbtnCancelar.Size = new System.Drawing.Size(26, 26);
             this.tsbtnCancelar.Text = "Cancelar";
             // 
+            // toolStripSeparator
+            // 
+            this.toolStripSeparator.Name = "toolStripSeparator";
+            this.toolStripSeparator.Size = new System.Drawing.Size(6, 29);
+            // 
             // tsbtnImprimir
             // 
             this.tsbtnImprimir.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
@@ -127,6 +120,11 @@
             this.tsbtnImprimir.Name = "tsbtnImprimir";
             this.tsbtnImprimir.Size = new System.Drawing.Size(26, 26);
             this.tsbtnImprimir.Text = "Imprimir";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 29);
             // 
             // tsbtnSair
             // 
@@ -138,6 +136,16 @@
             this.tsbtnSair.Text = "Sair";
             this.tsbtnSair.Click += new System.EventHandler(this.tsbtnSair_Click);
             // 
+            // txtPesquisa
+            // 
+            this.txtPesquisa.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPesquisa.Location = new System.Drawing.Point(76, 32);
+            this.txtPesquisa.Name = "txtPesquisa";
+            this.txtPesquisa.Size = new System.Drawing.Size(257, 20);
+            this.txtPesquisa.TabIndex = 1;
+            this.txtPesquisa.TextChanged += new System.EventHandler(this.txtPesquisa_TextChanged);
+            // 
             // lblFornecedor
             // 
             this.lblFornecedor.AutoSize = true;
@@ -147,14 +155,38 @@
             this.lblFornecedor.TabIndex = 11;
             this.lblFornecedor.Text = "Fornecedor:";
             // 
-            // txtPesquisa
+            // lblPeriodo
             // 
-            this.txtPesquisa.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtPesquisa.Location = new System.Drawing.Point(76, 32);
-            this.txtPesquisa.Name = "txtPesquisa";
-            this.txtPesquisa.Size = new System.Drawing.Size(307, 20);
-            this.txtPesquisa.TabIndex = 1;
+            this.lblPeriodo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblPeriodo.AutoSize = true;
+            this.lblPeriodo.Location = new System.Drawing.Point(417, 35);
+            this.lblPeriodo.Name = "lblPeriodo";
+            this.lblPeriodo.Size = new System.Drawing.Size(48, 13);
+            this.lblPeriodo.TabIndex = 12;
+            this.lblPeriodo.Text = "Período:";
+            // 
+            // mtxDtInicial
+            // 
+            this.mtxDtInicial.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.mtxDtInicial.Location = new System.Drawing.Point(471, 32);
+            this.mtxDtInicial.Mask = "00/00/0000";
+            this.mtxDtInicial.Name = "mtxDtInicial";
+            this.mtxDtInicial.Size = new System.Drawing.Size(75, 20);
+            this.mtxDtInicial.TabIndex = 2;
+            this.mtxDtInicial.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.mtxDtInicial.ValidatingType = typeof(System.DateTime);
+            // 
+            // mtxDtFinal
+            // 
+            this.mtxDtFinal.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.mtxDtFinal.Location = new System.Drawing.Point(552, 32);
+            this.mtxDtFinal.Mask = "00/00/0000";
+            this.mtxDtFinal.Name = "mtxDtFinal";
+            this.mtxDtFinal.Size = new System.Drawing.Size(75, 20);
+            this.mtxDtFinal.TabIndex = 3;
+            this.mtxDtFinal.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.mtxDtFinal.ValidatingType = typeof(System.DateTime);
+            this.mtxDtFinal.Leave += new System.EventHandler(this.mtxDtFinal_Leave);
             // 
             // pnlConsCompras
             // 
@@ -163,7 +195,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pnlConsCompras.Controls.Add(this.dgvCompras);
             this.pnlConsCompras.Controls.Add(this.mtxDtFinal);
-            this.pnlConsCompras.Controls.Add(this.txtDtInicial);
+            this.pnlConsCompras.Controls.Add(this.mtxDtInicial);
             this.pnlConsCompras.Controls.Add(this.lblPeriodo);
             this.pnlConsCompras.Controls.Add(this.lblFornecedor);
             this.pnlConsCompras.Controls.Add(this.txtPesquisa);
@@ -173,35 +205,103 @@
             this.pnlConsCompras.Size = new System.Drawing.Size(983, 590);
             this.pnlConsCompras.TabIndex = 2;
             // 
-            // mtxDtFinal
+            // dgvCompras
             // 
-            this.mtxDtFinal.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.mtxDtFinal.Location = new System.Drawing.Point(543, 32);
-            this.mtxDtFinal.Mask = "00/00/0000";
-            this.mtxDtFinal.Name = "mtxDtFinal";
-            this.mtxDtFinal.Size = new System.Drawing.Size(80, 20);
-            this.mtxDtFinal.TabIndex = 3;
-            this.mtxDtFinal.ValidatingType = typeof(System.DateTime);
+            this.dgvCompras.AllowUserToAddRows = false;
+            this.dgvCompras.AllowUserToDeleteRows = false;
+            this.dgvCompras.AllowUserToOrderColumns = true;
+            this.dgvCompras.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvCompras.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvCompras.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCompras.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.fornecedor,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.vendedor});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvCompras.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvCompras.Location = new System.Drawing.Point(3, 58);
+            this.dgvCompras.Name = "dgvCompras";
+            this.dgvCompras.ReadOnly = true;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvCompras.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvCompras.RowHeadersWidth = 12;
+            this.dgvCompras.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvCompras.Size = new System.Drawing.Size(977, 529);
+            this.dgvCompras.TabIndex = 14;
             // 
-            // txtDtInicial
+            // id
             // 
-            this.txtDtInicial.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtDtInicial.Location = new System.Drawing.Point(457, 32);
-            this.txtDtInicial.Mask = "00/00/0000";
-            this.txtDtInicial.Name = "txtDtInicial";
-            this.txtDtInicial.Size = new System.Drawing.Size(80, 20);
-            this.txtDtInicial.TabIndex = 2;
-            this.txtDtInicial.ValidatingType = typeof(System.DateTime);
+            this.id.DataPropertyName = "id";
+            this.id.HeaderText = "Id";
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
             // 
-            // lblPeriodo
+            // fornecedor
             // 
-            this.lblPeriodo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblPeriodo.AutoSize = true;
-            this.lblPeriodo.Location = new System.Drawing.Point(405, 35);
-            this.lblPeriodo.Name = "lblPeriodo";
-            this.lblPeriodo.Size = new System.Drawing.Size(46, 13);
-            this.lblPeriodo.TabIndex = 12;
-            this.lblPeriodo.Text = "Periodo:";
+            this.fornecedor.DataPropertyName = "fornecedor";
+            this.fornecedor.HeaderText = "Fornecedor";
+            this.fornecedor.Name = "fornecedor";
+            this.fornecedor.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "valor";
+            this.Column3.HeaderText = "Valor";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.DataPropertyName = "desconto";
+            this.Column4.HeaderText = "Desconto";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.DataPropertyName = "valor_total";
+            this.Column5.HeaderText = "Valor total";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // Column6
+            // 
+            this.Column6.DataPropertyName = "data_compra";
+            this.Column6.HeaderText = "Dt da compra";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            // 
+            // vendedor
+            // 
+            this.vendedor.DataPropertyName = "usuario";
+            this.vendedor.HeaderText = "Usuário";
+            this.vendedor.Name = "vendedor";
+            this.vendedor.ReadOnly = true;
             // 
             // FrmConsCompras
             // 
@@ -215,31 +315,38 @@
             this.Text = "Consulta de Compras";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FrmConsCompras_KeyDown);
-            ((System.ComponentModel.ISupportInitialize)(this.dgvCompras)).EndInit();
             this.tsCompra.ResumeLayout(false);
             this.tsCompra.PerformLayout();
             this.pnlConsCompras.ResumeLayout(false);
             this.pnlConsCompras.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCompras)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dgvCompras;
-        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
-        private System.Windows.Forms.ToolStripSeparator toolStripSeparator;
         private System.Windows.Forms.ToolStrip tsCompra;
         private System.Windows.Forms.ToolStripButton tsbtnAddCompra;
         private System.Windows.Forms.ToolStripButton tsbtnEditCompra;
         private System.Windows.Forms.ToolStripButton tsbtnCancelar;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator;
         private System.Windows.Forms.ToolStripButton tsbtnImprimir;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolStripButton tsbtnSair;
-        private System.Windows.Forms.Label lblFornecedor;
         private System.Windows.Forms.TextBox txtPesquisa;
-        private System.Windows.Forms.Panel pnlConsCompras;
-        private System.Windows.Forms.MaskedTextBox mtxDtFinal;
-        private System.Windows.Forms.MaskedTextBox txtDtInicial;
+        private System.Windows.Forms.Label lblFornecedor;
         private System.Windows.Forms.Label lblPeriodo;
+        private System.Windows.Forms.MaskedTextBox mtxDtInicial;
+        private System.Windows.Forms.MaskedTextBox mtxDtFinal;
+        private System.Windows.Forms.Panel pnlConsCompras;
+        private System.Windows.Forms.DataGridView dgvCompras;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fornecedor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn vendedor;
     }
 }
--- Projeto Novo/Compras/ConsCompras.cs
+++ Projeto Novo/Compras/ConsCompras.cs
@@ -7,15 +7,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Projeto_Novo
 {
     public partial class FrmConsCompras : Form
     {
+        Conexao con = new Conexao();
+        MySqlCommand cmd;
+
         public FrmConsCompras()
         {
             InitializeComponent();
+
+            this.Consulta();
         }
+
+        private void Consulta()
+        {
+            con.OpenConn();
+
+            if (mtxDtInicial.MaskCompleted && mtxDtFinal.MaskCompleted)
+            {
+                try
+                {
+                    cmd = new MySqlCommand("SELECT a.ID, a.FORNECEDOR, a.VALOR, a.DESCONTO, a.VALOR_TOTAL, a.DATA_COMPRA, b.NOME " +
+                        "FROM COMPRA a LEFT JOIN FUNCIONARIO b ON a.USUARIO = b.ID WHERE EX = 0 AND DATA_COMPRA BETWEEN" +
+                        $" (SELECT date_format(str_to_date('{mtxDtInicial.Text}', '%d/%m/%Y'), '%Y-%m-%d'))" +
+                        $" AND (SELECT date_format(str_to_date('{mtxDtFinal.Text}', '%d/%m/%Y'), '%Y-%m-%d') AND FORNECEDOR LIKE '%{txtPesquisa.Text}%');", con.query);
+                    MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                    DataSet ds = new DataSet();
+                    da.Fill(ds);
+                    dgvCompras.DataSource = ds;
+                    dgvCompras.DataMember = ds.Tables[0].TableName;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error: {ex.Message}", "AmSystem", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                cmd = new MySqlCommand("SELECT a.ID, a.FORNECEDOR, a.VALOR, a.DESCONTO, a.VALOR_TOTAL, a.DATA_COMPRA, b.NOME " +
+                    $"FROM COMPRA a LEFT JOIN FUNCIONARIO b ON a.USUARIO = b.ID WHERE EX = 0 AND FORNECEDOR LIKE '%{txtPesquisa.Text}%';", con.query);
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                dgvCompras.DataSource = ds;
+                dgvCompras.DataMember = ds.Tables[0].TableName;
+            }
+            con.CloseConn();
+        }
+
         private void FrmConsCompras_KeyDown(object sender, KeyEventArgs e)
         {
             switch (e.KeyCode)
@@ -23,6 +66,10 @@ namespace Projeto_Novo
                 case Keys.Enter:
                     e.SuppressKeyPress = true;
                     this.SelectNextControl(ActiveControl, !e.Shift, true, true, true);
+                    break;
+
+                case Keys.F3:
+                    tsbtnAddCompra_Click(sender, e);
                     break;
 
                 case Keys.Escape:
@@ -47,6 +94,16 @@ namespace Projeto_Novo
         private void tsbtnSair_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void txtPesquisa_TextChanged(object sender, EventArgs e)
+        {
+            Consulta();
+        }
+
+        private void mtxDtFinal_Leave(object sender, EventArgs e)
+        {
+            Consulta();
         }
     }
 }
